d2224ba20594c9aa7dfb11863dab2e96deeaecb1
markhenning <markhenning@outlook.com>
2020-06-08 23:32:26 +0100

    Updated with parameters

--- Decomm-Server.ps1
+++ Decomm-Server.ps1
@@ -17,8 +17,14 @@ Function Get-RemoveADDNSEntry ($computerName) {
 }
 
 
-#>Function to Start all this madness, go get the server object
-Function Get-RemoveADComputer ($computerName) {
+
+
+
+Function Get-RemoveADComputer {
+    param (
+        [Parameter(Mandatory)][string]$computerName,
+        [bool]$PauseAfterDisable = $true
+    )
     
     ## Go get objects with the right name from the domain
     $computerObject = Get-AdComputer -filter { Name -like $computerName }
@@ -29,14 +35,19 @@ Function Get-RemoveADComputer ($computerName) {
     ## Test we only got one, otherwise exit and complain
     if ( $count -eq 1 ) {
 
+        Write-Host ""
         Write-Host "Working on:" $computerObject.Name
+        Write-Host ""
 
         ## Safety first -disable the account, we can use this as an extra safety later and "only delete 1 account that's disabled"
         $computerObject | Disable-ADAccount -erroraction SilentlyContinue
        
         # If you uncomment the following two lines, the script will hold here with a message on screen so you can check if it disabled the right arround
-        ##Write-Host "Pause before deletion"
-        ##pause
+        if ($PauseAfterDisable){
+            Write-Host "Pausing after disabling, but before deleting"
+            Write-Host "Please confirm we disabled the right computer account"
+            pause
+        }
 
         ## Right, I have no idea why it's necessary to pull this again, but it doesn't see the disable unless we do
         $computerObject = Get-AdComputer -filter { Name -like $computerName }
@@ -55,8 +66,10 @@ Function Get-RemoveADComputer ($computerName) {
 
     } else {
 
-        Write-Host "Error, expected 1 object, got $count"
+        Write-Host "Error, was looking for 1 server, but found $count"
 
     }
 
 }
+
+
